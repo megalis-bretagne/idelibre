00a626f5667fda8cbfdecd17fbfb3d8f0ff47f09
rdubourget <remi.dubourget@libriciel.coop>
2020-10-04 15:27:41 +0200

    entity + form

---
+++ migrations/Version20201004132106.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201004132106 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SEQUENCE gdpr_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE gdpr (id INT NOT NULL, company_name VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, representative VARCHAR(255) NOT NULL, quality VARCHAR(255) NOT NULL, siret VARCHAR(255) NOT NULL, ape VARCHAR(255) NOT NULL, company_phone VARCHAR(255) NOT NULL, company_email VARCHAR(255) NOT NULL, dpo_email VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE gdpr_id_seq CASCADE');
+        $this->addSql('DROP TABLE gdpr');
+    }
+}
