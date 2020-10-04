62542b20b6b7a4e7ca342f9dd6adfb63b9d8f561
rdubourget <remi.dubourget@libriciel.coop>
2020-10-04 16:13:44 +0200

    missing tests

--- src/Controller/GdprController.php
+++ src/Controller/GdprController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\GdprType;
 use App\Service\Gdpr\GdprManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GdprController extends AbstractController
@@ -11,7 +15,7 @@ class GdprController extends AbstractController
     /**
      * @Route("/gdpr/notice", name="gdpr_notice")
      */
-    public function notice(GdprManager $gdprManager)
+    public function notice(GdprManager $gdprManager):Response
     {
         return $this->render('gdpr/notice.html.twig', [
             'gdpr' => $gdprManager->getGdpr()
@@ -21,11 +25,23 @@ class GdprController extends AbstractController
 
     /**
      * @Route("/gdpr/edit", name="gdpr_edit")
+     * @IsGranted("ROLE_SUPERADMIN")
      */
-    public function edit(GdprManager $gdprManager)
+    public function edit(GdprManager $gdprManager, Request $request):Response
     {
+        $form = $this->createForm(GdprType::class, $gdprManager->getGdpr());
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $gdprManager->save($form->getData());
 
-            $gdprManager->getGdpr()
+            $this->addFlash('success', 'Vos données RGPD ont été mise à jour');
+            return $this->redirectToRoute('gdpr_notice');
+        }
+
+        return $this->render('gdpr/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+
 
     }
 
