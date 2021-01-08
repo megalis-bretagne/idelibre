65f9a293a9232a6abba0f9289723f6b4bacac0a2
rdubourget <remi.dubourget@libriciel.coop>
2021-01-08 16:14:22 +0100

    fix select2 resize

--- public/assets/enableSelect2.js
+++ public/assets/enableSelect2.js
@@ -2,7 +2,6 @@ $('document').ready(function () {
     $('select').select2({
         'closeOnSelect': true,
         'allowClear': true,
-        //placeholder: "Sélectionner les destinataires",
-      //  'minimumInputLength': 2
+         width: '100%'
     });
 });
