8662e5f472f567a49c6f78181268aca65d481e74
rdubourget <remi.dubourget@libriciel.coop>
2021-02-18 22:23:02 +0100

    lsFileType

---
+++ src/Form/LsFileType.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class LsFile extends AbstractType
+{
+    public function getParent(): ?string
+    {
+        return FileType::class;
+    }
+
+}
