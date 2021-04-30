4804ec17a3a477143696d62adfd800e54a0be533
rdubourget <remi.dubourget@libriciel.coop>
2021-04-30 11:08:42 +0200

    done

---
+++ src/EventListener/PartySaveNotifier.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\EventListener;
+
+
+use App\Entity\Party;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Event\PreFlushEventArgs;
+
+class PartySaveNotifier
+{
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function preFlush(Party $party, PreFlushEventArgs $event): void
+    {
+        if($party->getLegacyId() !== null) {
+            return;
+        }
+
+        if ('test' === getenv('APP_ENV')) {
+            $party->setLegacyId(99999);
+            return;
+        }
+
+        $conn = $this->em->getConnection();
+        $sql = "select nextval('party_legacy_seq');";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $nextVal = $stmt->fetchOne();
+
+        $party->setLegacyId($nextVal);
+    }
+}
