fdd440a46e1ab92c61e31f6fc86c50da00166d79
rdubourget <remi.dubourget@libriciel.coop>
2021-05-05 16:05:57 +0200

    test ok

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -129,7 +129,7 @@ class UserController extends AbstractController
     public function deleteBatch(UserRepository $userRepository, Request $request): Response
     {
         if ($request->isMethod('POST')) {
-            $deleted = $userRepository->deleteActorsByStructure($this->getUser()->getStructure(), $request->request->get('users') ?? []);
+            $userRepository->deleteActorsByStructure($this->getUser()->getStructure(), $request->request->get('users') ?? []);
             $this->addFlash('success', 'Les élus ont été supprimés.');
 
             return $this->redirectToRoute('user_index');
