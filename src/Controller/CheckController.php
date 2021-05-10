b0608da23a936281abac41471140af437da3dc6b
rdubourget <remi.dubourget@libriciel.coop>
2021-05-10 15:47:59 +0200

    add tests

--- src/Controller/CheckController.php
+++ src/Controller/CheckController.php
@@ -42,13 +42,13 @@ class CheckController extends AbstractController
             'memory_limit' => ini_get('memory_limit'),
             'post_max_size' => ini_get('post_max_size'),
             'upload_max_filesize' => ini_get('upload_max_filesize'),
-            'max_file_uploads' => ini_get('max_file_uploads')
+            'max_file_uploads' => ini_get('max_file_uploads'),
         ];
 
         return $this->render('check/index.html.twig', [
             'isNodejs' => $isNodejs,
             'isLshorodatage' => $isLshorodatage,
-            'phpConfig' => $phpConfig
+            'phpConfig' => $phpConfig,
         ]);
     }
 
---
+++ tests/Controller/CheckControllerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\DataFixtures\UserFixtures;
+use App\Tests\FindEntityTrait;
+use App\Tests\LoginTrait;
+use Doctrine\Persistence\ObjectManager;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class CheckControllerTest extends WebTestCase
+{
+    use FixturesTrait;
+    use FindEntityTrait;
+    use LoginTrait;
+
+
+    private ?KernelBrowser $client;
+    /**
+     * @var ObjectManager
+     */
+    private $entityManager;
+
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+
+        $kernel = self::bootKernel();
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $this->loadFixtures([
+            UserFixtures::class
+        ]);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->client = null;
+        $this->entityManager->close();
+    }
+
+    public function testIndex()
+    {
+        $this->loginAsSuperAdmin();
+        $crawler = $this->client->request(Request::METHOD_GET, '/check');
+        $this->assertResponseStatusCodeSame(200);
+
+        $item = $crawler->filter('html:contains("Vérification de la plateforme")');
+        $this->assertCount(1, $item);
+    }
+
+
+}
