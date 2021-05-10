7b744d0e260199736efd52a8dfe7d2f7fa53c8ef
rdubourget <remi.dubourget@libriciel.coop>
2021-05-10 09:13:59 +0200

    fix reset

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -97,6 +97,7 @@ class SecurityController extends AbstractController
     {
         if ($request->isMethod('post')) {
             $username = $request->request->get('username');
+
             try {
                 $resetPassword->reset($username);
             } catch (EntityNotFoundException $e) {
