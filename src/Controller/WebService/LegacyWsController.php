27b2f6c479f5eda11382363f36704986741c2c7d
rdubourget <remi.dubourget@libriciel.coop>
2021-05-11 09:42:08 +0200

    add legacy_connection

--- src/Controller/WebService/LegacyWsController.php
+++ src/Controller/WebService/LegacyWsController.php
@@ -1,22 +1,16 @@
 <?php
 
-
 namespace App\Controller\WebService;
-
 
 use Symfony\Component\Routing\Annotation\Route;
 
 class LegacyWsController
 {
-
     /**
      * @Route("/seance.json", name="wd_connector")
-     *
      */
     public function addSitting()
     {
         dd('ok');
     }
-
-
 }
