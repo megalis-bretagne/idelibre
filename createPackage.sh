6716caf56475e31f12dc5546d8ad98e6e641eca5
rdubourget <remi.dubourget@libriciel.coop>
2021-06-02 11:23:23 +0200

    fix typo again

--- createPackage.sh
+++ createPackage.sh
@@ -7,4 +7,4 @@ rm -rf $dest
 
 mkdir $dest
 
-cp -rfa docker-compose.yml .env docker-resources initializeCertificate.sh  $dest
+cp -rfa docker-compose.yml .env docker-resources initializeCertificates.sh  $dest
