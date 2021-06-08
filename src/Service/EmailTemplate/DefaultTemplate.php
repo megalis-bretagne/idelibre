d69a58168620997e7f634e909b6fa099a5fecd10
rdubourget <remi.dubourget@libriciel.coop>
2021-06-08 16:57:18 +0200

    remove unless reset pasword tempate

--- src/Service/EmailTemplate/DefaultTemplate.php
+++ src/Service/EmailTemplate/DefaultTemplate.php
@@ -4,10 +4,6 @@ namespace App\Service\EmailTemplate;
 
 class DefaultTemplate
 {
-    public const FORGET_PASSWORD = 'Bonjour, <br>' . PHP_EOL . // NOSONAR
-'Vous avez effectué une demande de remise à zéro de mot de passe <br>
-Veuillez Cliquer ici pour le réinitialiser #reinitLink#';
-
     public const CONVOCATION = 'Bonjour, <br>
 Vous êtes convoqué à la séance ... <br> ';
 
