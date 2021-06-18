d2383cfe709d7b21fa7e2a224dd5ebc24fca525a
rdubourget <remi.dubourget@libriciel.coop>
2021-06-18 08:43:26 +0200

    generate pdfSitting and zip from Webservice

--- src/Controller/WebService/LegacyWsController.php
+++ src/Controller/WebService/LegacyWsController.php
@@ -13,7 +13,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\Messenger\MessageBus;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
