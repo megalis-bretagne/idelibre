de08b43f051603fa0732b5da6c0c55d44556a946
rdubourget <remi.dubourget@libriciel.coop>
2021-08-26 16:24:48 +0200

    sentry

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
