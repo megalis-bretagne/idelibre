69da4e4405d4e27faa4813e0dde7f9b7812075c4
rdubourget <remi.dubourget@libriciel.coop>
2021-09-29 12:00:48 +0200

    calendar fixture

---
+++ src/DataFixtures/CalendarFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Annex;
+use App\Entity\Calendar;
+use App\Entity\Type;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class CalendarFixtures extends Fixture implements DependentFixtureInterface
+{
+    public const REFERENCE = 'Calendar_';
+
+    public function load(ObjectManager $manager): void
+    {
+        /** @var Type $typeConseilLibriciel */
+        $typeConseilLibriciel = $this->getReference(TypeFixtures::REFERENCE . 'conseilLibriciel');
+
+        $calendarConseil = (new Calendar())
+            ->setType($typeConseilLibriciel)
+            ->setIsActive(true)
+            ->setDuration(240);
+
+        $manager->persist($calendarConseil);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            TypeFixtures::class
+        ];
+    }
+}
