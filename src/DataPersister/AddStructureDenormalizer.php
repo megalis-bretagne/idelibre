98997a0f181bfe63949287c32cf4e8c8bf309028
rdubourget <remi.dubourget@libriciel.coop>
2021-10-11 09:30:16 +0200

    add status

---
+++ src/DataPersister/AddStructureDenormalizer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\DataPersister;
+
+use App\Entity\Structure;
+use App\Entity\User;
+use Psr\Log\LoggerInterface;
+use ReflectionClass;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Serializer\Normalizer\ContextAwareDenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+
+class AddStructureDenormalizer implements ContextAwareDenormalizerInterface, DenormalizerAwareInterface
+{
+    use DenormalizerAwareTrait;
+
+    private const ALREADY_CALLED_DENORMALIZER = 'AddStructureDenormalizerCalled';
+
+    public function __construct(private LoggerInterface $logger, private Security $security)
+    {
+    }
+
+    public function supportsDenormalization($data, string $type, string $format = null, array $context = []): bool
+    {
+        if (!empty($context[self::ALREADY_CALLED_DENORMALIZER])) {
+            return false;
+        }
+
+        if(!(isset($context['collection_operation_name'])) || $context['collection_operation_name'] !== "post") {
+            return false;
+        }
+
+        try {
+            return (new ReflectionClass($type))->hasMethod('setStructure');
+        } catch (\ReflectionException $e) {
+            $this->logger->error($e);
+            return false;
+        }
+    }
+
+    public function denormalize($data, string $type, string $format = null, array $context = []): User
+    {
+        $context[self::ALREADY_CALLED_DENORMALIZER] = true;
+
+        $structurableObject = $this->denormalizer->denormalize($data, $type, $format, $context);
+
+        $structurableObject->setStructure($this->getCurrentUserStructure());
+
+        return $structurableObject;
+    }
+
+    private function getCurrentUserStructure(): Structure
+    {
+        return $this->security->getUser()->getStructure();
+    }
+
+}
