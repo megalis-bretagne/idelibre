1975d33a8320a9b15355656726e99b31d44c201b
rdubourget <remi.dubourget@libriciel.coop>
2021-10-11 09:55:26 +0200

    limit to sctructure

--- src/Doctrine/CurrentStructureExtension.php
+++ src/Doctrine/CurrentStructureExtension.php
@@ -5,8 +5,9 @@ namespace App\Doctrine;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\Structure;
-use App\Entity\User;
 use Doctrine\ORM\QueryBuilder;
+use ReflectionClass;
+use ReflectionException;
 use Symfony\Component\Security\Core\Security;
 
 class CurrentStructureExtension implements QueryCollectionExtensionInterface
@@ -18,9 +19,14 @@ class CurrentStructureExtension implements QueryCollectionExtensionInterface
         $this->security = $security;
     }
 
+    /**
+     * @throws ReflectionException
+     */
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
-        if (User::class === $resourceClass) {
+        $class = new ReflectionClass($resourceClass);
+
+        if ($class->hasProperty('structure')) {
             $alias = $queryBuilder->getRootAliases()[0];
             $queryBuilder->andWhere("$alias.structure = :currentStructure")
                 ->setParameter('currentStructure', $this->getStructure());
