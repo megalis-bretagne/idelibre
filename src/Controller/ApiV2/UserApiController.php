7b38a8deb2642aa17262546370b3dfc75fb2026d
rdubourget <remi.dubourget@libriciel.coop>
2021-10-25 11:30:18 +0200

    userOk

--- src/Controller/ApiV2/UserApiController.php
+++ src/Controller/ApiV2/UserApiController.php
@@ -11,7 +11,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
