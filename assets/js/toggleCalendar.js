7a9ff3fd12d3b1f57e31503ecd281b0fd44f6659
rdubourget <remi.dubourget@libriciel.coop>
2021-12-08 11:33:41 +0100

    fix reminder new sitting

--- assets/js/toggleCalendar.js
+++ assets/js/toggleCalendar.js
@@ -1,5 +1,11 @@
 import $ from 'jquery';
 
+$("document").ready(function () {
+    if(!$("#sitting_reminder_isActive").attr('checked')) {
+        $('#sitting_reminder_duration').attr('disabled', true);
+    }
+})
+
 
 $("#sitting_reminder_isActive").change(function (event) {
     let $reminderDuration = $('#sitting_reminder_duration');
--- src/Form/ReminderSittingType.php
+++ src/Form/ReminderSittingType.php
@@ -7,48 +7,31 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReminderSittingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
-            /** @var Reminder|null $reminder */
-            $reminder = $event->getData();
-            $form = $event->getForm();
 
-            $form
+
+            $builder
                 ->add('isActive', CheckboxType::class, [
                     'required' => false,
                     'label_attr' => ['class' => 'switch-custom'],
                     'label' => 'Ajouter au calendrier',
-                    'data' => $reminder ? $reminder->getIsActive() : false,
                 ])
                 ->add('duration', ChoiceType::class, [
                     'label' => 'Durée',
-                    'disabled' => !$this->isActive($reminder),
                     'choices' => Reminder::VALUES,
-                    'data' => $reminder ? $reminder->getDuration() : 120,
                 ]);
-        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Reminder::class,
+            'sittingData' => null
         ]);
-    }
-
-    private function isActive(?Reminder $reminder): bool
-    {
-        if ($reminder) {
-            return $reminder->getIsActive();
-        }
-
-        return false;
     }
 }
