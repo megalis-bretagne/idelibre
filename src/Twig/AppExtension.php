e08788e9d0c7e357070d7b2ed92f8af097afbac7
rdubourget <remi.dubourget@libriciel.coop>
2021-12-09 10:46:17 +0100

    minutesOrHourFormating

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -11,11 +11,23 @@ class AppExtension extends AbstractExtension
     {
         return [
             new TwigFilter('noSuffix', [$this, 'removeSuffix']),
+            new TwigFilter('minutesOrHours', [$this, 'formatMinutesOrHours']),
         ];
     }
 
     public function removeSuffix(string $username): string
     {
         return preg_replace('/@.*/', '', $username);
+    }
+
+    public function formatMinutesOrHours(string $timeInMinutes): string
+    {
+        if($timeInMinutes % 60 != 0) {
+            return $timeInMinutes . ' minutes';
+        }
+
+        $timeInHours = $timeInMinutes / 60;
+
+        return $timeInHours > 1 ? $timeInHours . ' heures' : $timeInHours . ' heure';
     }
 }
