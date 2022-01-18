a0f36fd2429718eb6461fbf9f882274f05f8f824
rdubourget <remi.dubourget@libriciel.coop>
2022-01-18 09:23:08 +0100

    remove delete sequence from migration

--- migrations/Version20211005090129.php
+++ migrations/Version20211005090129.php
@@ -22,7 +22,6 @@ final class Version20211005090129 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('DROP SEQUENCE party_legacy_seq CASCADE');
         $this->addSql('ALTER TABLE "timestamp" ADD sitting_id UUID DEFAULT NULL');
         $this->addSql('ALTER TABLE "timestamp" ADD CONSTRAINT FK_A5D6E63E8014E66 FOREIGN KEY (sitting_id) REFERENCES sitting (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_A5D6E63E8014E66 ON "timestamp" (sitting_id)');
@@ -34,7 +33,6 @@ final class Version20211005090129 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('CREATE SEQUENCE party_legacy_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('ALTER TABLE timestamp DROP CONSTRAINT FK_A5D6E63E8014E66');
         $this->addSql('DROP INDEX IDX_A5D6E63E8014E66');
         $this->addSql('ALTER TABLE timestamp DROP sitting_id');
