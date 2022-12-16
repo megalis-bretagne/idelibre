590cb3924f9977e114e19be2173707c31baf27b6
aauzolat <2fev2!el2dec0!na>
2022-12-16 17:03:03 +0100

    Correction schéma doctrine

--- migrations/Version20221215092117.php
+++ migrations/Version20221215092117.php
@@ -20,15 +20,13 @@ final class Version20221215092117 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE party_legacy_seq CASCADE');
-        $this->addSql('ALTER TABLE "user" ADD accept_mail_recap BOOLEAN DEFAULT \'false\' NOT NULL');
+        $this->addSql('ALTER TABLE "user" ADD accept_mail_recap BOOLEAN NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('CREATE SEQUENCE party_legacy_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('ALTER TABLE "user" DROP accept_mail_recap');
     }
 }
