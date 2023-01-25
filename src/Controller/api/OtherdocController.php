3a4f7ed97d3c426e91988a58084302711a791ac7
Arnaud AUZOLAT <arnaud.auzolat@libriciel.coop>
2023-01-25 13:23:46 +0100

    Application du contrôle des pdfs sur les otherdocs également

--- src/Controller/api/OtherdocController.php
+++ src/Controller/api/OtherdocController.php
@@ -23,9 +23,14 @@ class OtherdocController extends AbstractController
     {
         $rawOtherdocs = $request->request->get('otherdocs');
         $otherdocs = $serializer->deserialize($rawOtherdocs, OtherdocApi::class . '[]', 'json');
-//        dd($otherdocs);
+
         if (!$pdfValidator->isOtherdocsPdf($otherdocs)) {
             return $this->json(['success' => false, 'message' => 'Au moins un document n\'est pas un pdf'], 400);
+        }
+        foreach ($pdfValidator->isOpenablePdf() as $otherdocUploaded => $allowed) {
+            if( !$allowed ) {
+                return $this->json(['success' => false, 'message' => "Le fichier ".$otherdocUploaded.' n\'est pas valide'], 400);
+            }
         }
         $otherdocManager->update($otherdocs, $request->files->all(), $sitting);
         $messageBus->dispatch(new UpdatedSitting($sitting->getId()));
