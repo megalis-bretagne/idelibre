8ea2dcc444d48d2f8a572ef8f05c70853cccd44b
Arnaud AUZOLAT <arnaud.auzolat@libriciel.coop>
2023-01-30 16:09:50 +0100

    Correction du test en lien avec la modification du pdfvalidator

--- tests/Service/Pdf/PdfValidatorTest.php
+++ tests/Service/Pdf/PdfValidatorTest.php
@@ -190,7 +190,17 @@ class PdfValidatorTest extends WebTestCase
     public function testIsPdfContent()
     {
         $uploadedFile1 = new UploadedFile(__DIR__ . '/../../resources/pdfEncrypted.pdf', 'pdfEncrypted.pdf');
-        $this->assertTrue($this->pdfvalidator->isPdfContent(file_get_contents($uploadedFile1)));
+        $openDoc = fopen($uploadedFile1->getPathname(), 'r');
+        while( !feof($openDoc) ) {
+            $lines[] = fgets($openDoc);
+        }
+        $firstLine = $lines[0];
+        $lastLine = $lines[count($lines) - 1];
+        if( $lastLine === false ) {
+            $lastLine = $lines[count($lines) - 2];
+        }
+        fclose($openDoc);
+        $this->assertTrue($this->pdfvalidator->isPdfContent($firstLine, $lastLine) );
     }
 
     public function testIsNotProtectedByPasswordPdf()
