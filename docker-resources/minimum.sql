abf553292bd446b0bb19fda2d7e716ef8aac75d4
Rémi Dubourget <remi.dubourget@libriciel.coop>
2023-02-02 09:39:27 +0100

    fix minimum sql

--- docker-resources/minimum.sql
+++ docker-resources/minimum.sql
@@ -24,10 +24,10 @@ INSERT INTO role (id, name, composites, is_in_structure_role, pretty_name)
 VALUES ('811a1329-0cd0-49e8-b59b-5394219c50dd', 'Employee', '["ROLE_EMPLOYEE"]', true, 'Personnel administratif');
 
 
-INSERT INTO "user" (id, structure_id, email, username, role_id, password, first_name, last_name, is_active, accept_mail_recap)
+INSERT INTO "user" (id, structure_id, email, username, role_id, password, first_name, last_name, is_active)
 values (UUID_GENERATE_V4(), NULL, 'superadmin@exemple.org', 'superadminInstall', '94663bed-16e9-4990-8493-323c3d5e5565',
         '$argon2id$v=19$m=65536,t=4,p=1$jCNjXFnpctIdKy2XKJ3d9w$B2THO9hICaf20D73R6PB0FDiR1+2RpJCZlpG6RExTlg', 'super ',
-        'admin ', true, true);
+        'admin ', true);
 
 
 INSERT INTO timezone (id, name)
