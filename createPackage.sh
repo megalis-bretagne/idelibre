8134787eaf6ddcc6c6b5d9916fde1bb0ba0f4849
Rémi Dubourget <remi.dubourget@libriciel.coop>
2023-02-03 15:29:21 +0100

    improve createPackage

--- createPackage.sh
+++ createPackage.sh
@@ -7,6 +7,10 @@ rm -rf $dest
 
 mkdir $dest
 
-cp -rfa docker-compose.yml .env docker-resources initializeCertificates.sh  $dest
+cp -rfa docker-compose.yml .env.dist docker-resources initializeCertificates.sh  $dest
+
+rm ${dest}/docker-resources/initApp.sh ${dest}/docker-resources/initAppTest.sh ${dest}/docker-resources/minimum.sql ${dest}/docker-resources/opcache.ini ${dest}/docker-resources/zz-idelibre.ini
 
 sed -i -e"s|TAG|$CI_COMMIT_REF_NAME|" ${dest}/docker-compose.yml
+
+echo "generate installation package DONE"
