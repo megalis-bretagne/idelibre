3d8b1ad56073598b56196906c525c3fd8abc6114
Arnaud AUZOLAT <aauzolat@libriciel.coop>
2023-02-09 10:51:26 +0100

    Resolve "Force renew password"

--- src/Service/User/PasswordInvalidator.php
+++ src/Service/User/PasswordInvalidator.php
@@ -9,18 +9,18 @@ use App\Service\Email\EmailData;
 use App\Service\Email\EmailNotSendException;
 use App\Service\Email\EmailServiceInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class PasswordInvalidator
 {
     public const INVALID_PASSWORD = 'CHANGEZMOI';
-    public const INVALIDATE_PASSWORD_SUBJECT = 'Réinitialisation de votre mot de passe.';
-    public const INVALIDATE_PASSWORD_CONTENT = 'Votre mot de passe doit être changé afin de correspondre aux exigences de sécurité. 
-                    Veuillez utiliser l\'option "Mot de passe oublié" lors de votre prochaine connexion à nos services';
+    public const INVALIDATE_PASSWORD_SUBJECT = 'Réinitialisation de votre mot de passe';
 
     public function __construct(
         private EntityManagerInterface $em,
         private UserRepository $userRepository,
-        private EmailServiceInterface $emailService
+        private EmailServiceInterface $emailService,
+        private readonly ParameterBagInterface $bag,
     ) {
     }
 
@@ -48,14 +48,27 @@ class PasswordInvalidator
 
         foreach ($users as $user) {
             $subject = self::INVALIDATE_PASSWORD_SUBJECT;
-            $content = self::INVALIDATE_PASSWORD_CONTENT;
-
-            $emailData = new EmailData($subject, $content, EmailData::FORMAT_TEXT);
+            $content = $this->getContentMail($user);
+            $emailData = new EmailData($subject, $content, EmailData::FORMAT_HTML);
             $emailData->setTo($user->getEmail());
             $emailData->setReplyTo($replyTo);
             $emailsData[] = $emailData;
         }
 
         $this->emailService->sendBatch($emailsData);
+    }
+    private function getContentMail($user)
+    {
+        $prenom = $user->getFirstName();
+        $nom = $user->getLastName();
+        $productName = $this->bag->get('product_name');
+
+        return <<<HTML
+                <p>Bonjour $nom $prenom,</p>\r
+                <p>Une demande de réinitialisation de votre mot de passe a &eacute;t&eacute; faite par un administrateur de l'application $productName</p>\r
+                <p>Votre mot de passe doit être changé afin de correspondre aux exigences de sécurité.</p>\r
+                <p>Veuillez utiliser l'option "Mot de passe oublié" lors de votre prochaine connexion à nos services.</p>\r
+                <p>Merci</p>
+            HTML;
     }
 }
