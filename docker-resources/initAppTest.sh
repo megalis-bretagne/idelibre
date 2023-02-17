447546680ad3c7fbc7f548ab73e3fac56de950fb
Arnaud AUZOLAT <aauzolat@libriciel.coop>
2023-02-17 12:22:38 +0100

    Resolve "Dépôt des sources sur nexus et non plus ressources"

--- docker-resources/initAppTest.sh
+++
@@ -1,4 +0,0 @@
-echo "init databases with symfony"
-./bin/console doctrine:migrations:migrate --no-interaction
-./bin/console initBdd docker-resources/minimum.sql
-./bin/console cache:clear --no-interaction
