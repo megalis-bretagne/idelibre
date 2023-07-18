132880d90fda1917d39297b3628e8ec6ada85e71
Arnaud AUZOLAT <arnaud.auzolat@libriciel.coop>
2023-07-18 10:08:45 +0200

    Correction: ￼les mots de passe sont masqués et passés à vide dans le cas où le profil créé est un admin

--- assets/js/createUser.js
+++ assets/js/createUser.js
@@ -2,15 +2,21 @@ import $ from 'jquery';
 
 $('#user_role').change(function (e) {
     $('label[for=user_initPassword], #user_initPassword').show();
-    if ($(this).find(":selected").text() === 'Administrateur') {
-        $('label[for=user_initPassword], #user_initPassword').hide();
-        return;
-    }
-    if ($(this).find(":selected").text() === 'Elu') {
-        $('#actor-info').show();
-        return;
-    }
     $('#actor-info').hide();
     $('#user_party').val('');
     $('#user_title').val('');
+    $('#user_initPassword').val('0');
+    $('#user_plainPassword_first').parent().parent().hide();
+    $('#user_plainPassword_second').parent().parent().hide();
+
+    if ($(this).find(":selected").text() === 'Administrateur') {
+        $('label[for=user_initPassword], #user_initPassword').hide();
+        $('#user_plainPassword_first').parent().parent().hide();
+        $('#user_plainPassword_second').parent().parent().hide();
+        return;
+    }
+    else if ($(this).find(":selected").text() === 'Elu') {
+        $('#actor-info').show();
+        return;
+    }
 });
