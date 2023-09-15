3840855fb5e8ef8d5257cecf4ebee00f628062aa
pmonin <pmonin@libriciel.coop>
2023-09-15 14:26:18 +0200

    convocation issues

--- src/Entity/Convocation.php
+++ src/Entity/Convocation.php
@@ -86,11 +86,10 @@ class Convocation
     private ?AttendanceToken $attendanceToken = null;
 
     #[ORM\ManyToOne()]
-    //    #[ORM\ManyToOne( cascade: ['persist', 'remove'])]
     #[Groups(groups: ['user', 'convocation', 'convocation:read'])]
     private ?User $deputy = null;
 
-    #[ORM\ManyToOne(cascade: ['persist', 'remove'])]
+    #[ORM\ManyToOne()]
     #[Groups(groups: ['user', 'convocation', 'convocation:read'])]
     private ?User $mandator = null;
 
