2f61ec88d82aa72fc90b2f97578ea6bc25073394
Pierre-François MONIN <pmonin@libriciel.coop>
2023-09-21 11:09:54 +0200

    Resolve "Password invalidator et affectation des roles"

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -98,9 +98,6 @@ class UserController extends AbstractController
     #[Breadcrumb(title: 'Modifier {user.firstName} {user.lastName}')]
     public function edit(User $user, Request $request, UserManager $userManager): Response
     {
-        //        dd($this->userManager->AlreadyTakenDeputies($user));
-
-
         $form = $this->createForm(UserType::class, $user, [
             'structure' => $this->getUser()->getStructure(),
             'entropyForUser' => $this->getUser()->getStructure()->getConfiguration()->getMinimumEntropy(),
@@ -259,7 +256,6 @@ class UserController extends AbstractController
         $toExcludes = [];
 
         $toExcludes = [...$this->userManager->AlreadyTakenDeputies($user)];
-        //        dd($toExcludes);
         return $this->render('include/user_lists/_user_available_actors.html.twig', [
             "availables" => $this->userRepository->findDeputiesWithNoAssociation($this->getUser()->getStructure(), $toExcludes)->getQuery()->getResult(),
         ]);
