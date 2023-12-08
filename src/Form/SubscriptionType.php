62cde1709ffd592263d2b43302766f5c1f9df610
Pierre-François MONIN <pmonin@libriciel.coop>
2023-12-08 16:27:52 +0100

    Resolve "Formulaires"

--- src/Form/SubscriptionType.php
+++ src/Form/SubscriptionType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Subscription;
+use App\Form\Type\LsChoiceType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,11 +13,16 @@ class SubscriptionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $subscription = $builder->getData();
         $builder
-            ->add('acceptMailRecap', CheckboxType::class, [
-                'required' => false,
-                'label_attr' => ['class' => 'checkbox-inline checkbox-switch'],
+            ->add('acceptMailRecap', LsChoiceType::class, [
                 'label' => 'Voulez-vous recevoir des mails récaptulatifs des présences/absences des élus ?',
+                'choices' => [
+                    'Oui' => true,
+                    'Non' => false,
+                ],
+                'data' => $subscription ? $subscription->getAcceptMailRecap() : false,
+                'required' => false,
             ])
         ;
     }
