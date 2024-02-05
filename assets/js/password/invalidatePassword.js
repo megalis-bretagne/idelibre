ea1a68ac7a09aa4980404eca76c2794783f2e0a4
pmonin <pmonin@libriciel.coop>
2024-02-05 16:30:18 +0100

    front

--- assets/js/password/invalidatePassword.js
+++ assets/js/password/invalidatePassword.js
@@ -7,7 +7,7 @@ const invalidateConfirmBtn = $('#invalidateConfirmBtn');
 const cancelBtn = $('#invalidateCancelBtn');
 const closeBtn = $('#close-invalidate')
 
-confirmInvalidation(invalidatePassword, invalidateConfirmBtn, dataInvalidate);
+secureConfirmation(invalidatePassword, invalidateConfirmBtn, dataInvalidate);
 clearInput(invalidateConfirmBtn, invalidatePassword, invalidateConfirmBtn);
 clearInput(cancelBtn, invalidatePassword, invalidateConfirmBtn);
 clearInput(closeBtn, invalidatePassword, invalidateConfirmBtn);
@@ -22,16 +22,36 @@ for (let i = 0; i < invalidateInputs.length; i++) {
     let cancelSingleBtn = invalidateInput.parents('.modal-content').find('.invalidateSingleCancelBtn');
     let closeSingleBtn = invalidateInput.parents('.modal-content').find('.close-invalidate-single');
 
-    confirmInvalidation(invalidateInput, invalidateSingleConfirmBtn, dataInvalidateSingle);
+    secureConfirmation(invalidateInput, invalidateSingleConfirmBtn, dataInvalidateSingle);
     clearInput(invalidateSingleConfirmBtn, invalidateInput, invalidateSingleConfirmBtn);
     clearInput(cancelSingleBtn, invalidateInput, invalidateSingleConfirmBtn);
     clearInput(closeSingleBtn, invalidateInput, invalidateSingleConfirmBtn);
 }
 
 
-function confirmInvalidation(target, confirmBtn, data) {
+// secure user deletion
+const deleteInputs = document.querySelectorAll('.delete-input');
+for (let i = 0; i < deleteInputs.length; i++) {
+
+        let deleteInput = $(deleteInputs[i]);
+        let dataDelete = $(deleteInputs[i]).data('delete');
+        let deleteConfirmBtn = deleteInput.parents('.modal-content').find('.delete-user-btn');
+        let cancelDeleteBtn = deleteInput.parents('.modal-content').find('.cancel-delete-user-btn');
+        let closeDeleteBtn = deleteInput.parents('.modal-content').find('.close-delete-user');
+
+        secureConfirmation(deleteInput, deleteConfirmBtn, dataDelete);
+        clearInput(deleteConfirmBtn, deleteInput, deleteConfirmBtn);
+        clearInput(cancelDeleteBtn, deleteInput, deleteConfirmBtn);
+        clearInput(closeDeleteBtn, deleteInput, deleteConfirmBtn);
+}
+
+
+
+function secureConfirmation(target, confirmBtn, data) {
     $(target).bind("input changes", function() {
         const value =  $(this).val();
+        console.log(data + " " + value)
+
 
         if(value === data) {
             confirmBtn.removeAttr('disabled');
