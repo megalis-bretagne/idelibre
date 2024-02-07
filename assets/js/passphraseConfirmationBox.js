da787fe0a1bf19486d733113460eb2a7017a0ceb
Rémi Dubourget <remi.dubourget@libriciel.coop>
2024-02-07 10:01:38 +0100

    fix timeout

--- assets/js/passphraseConfirmationBox.js
+++ assets/js/passphraseConfirmationBox.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 // secure invalidate multiple users password
-const invalidatePassword =$('#invalidate-password');
+const invalidatePassword = $('#invalidate-password');
 const dataInvalidate = invalidatePassword.data('invalidate');
 const invalidateConfirmBtn = $('#invalidate-confirm-btn');
 const cancelBtn = $('#invalidate-cancel-btn');
@@ -33,21 +33,21 @@ for (let i = 0; i < invalidateInputs.length; i++) {
 const deleteInputs = document.querySelectorAll('.delete-input');
 for (let j = 0; j < deleteInputs.length; j++) {
 
-        let deleteInput = $(deleteInputs[j]);
-        let dataDelete = $(deleteInputs[j]).data('delete');
-        let deleteConfirmBtn = deleteInput.parents('.modal-content').find('.delete-btn');
-        let cancelDeleteBtn = deleteInput.parents('.modal-content').find('.cancel-delete-btn');
-        let closeDeleteBtn = deleteInput.parents('.modal-content').find('.delete-close-btn');
+    let deleteInput = $(deleteInputs[j]);
+    let dataDelete = $(deleteInputs[j]).data('delete');
+    let deleteConfirmBtn = deleteInput.parents('.modal-content').find('.delete-btn');
+    let cancelDeleteBtn = deleteInput.parents('.modal-content').find('.cancel-delete-btn');
+    let closeDeleteBtn = deleteInput.parents('.modal-content').find('.delete-close-btn');
 
-        secureConfirmation(deleteInput, deleteConfirmBtn, dataDelete);
-        clearInput(deleteConfirmBtn, deleteInput, deleteConfirmBtn);
-        clearInput(cancelDeleteBtn, deleteInput, deleteConfirmBtn);
-        clearInput(closeDeleteBtn, deleteInput, deleteConfirmBtn);
+    secureConfirmation(deleteInput, deleteConfirmBtn, dataDelete);
+    clearInput(deleteConfirmBtn, deleteInput, deleteConfirmBtn);
+    clearInput(cancelDeleteBtn, deleteInput, deleteConfirmBtn);
+    clearInput(closeDeleteBtn, deleteInput, deleteConfirmBtn);
 }
 
 
 // secure multiple user deletion
-let deleteBatchInput =  $('#delete-batch-input')
+let deleteBatchInput = $('#delete-batch-input')
 let dataDelete = deleteBatchInput.data('delete');
 let deleteBatchConfirmBtn = deleteBatchInput.parents('.modal-content').find('#delete-batch-btn');
 let cancelDeleteBtn = deleteBatchInput.parents('.modal-content').find('#cancel-delete-batch-btn');
@@ -59,10 +59,10 @@ clearInput(cancelDeleteBtn, deleteBatchInput, deleteBatchConfirmBtn);
 clearInput(closeDeleteBatchBtn, deleteBatchInput, deleteBatchConfirmBtn);
 
 function secureConfirmation(target, confirmBtn, data) {
-    $(target).bind("input changes", function() {
-        const value =  $(this).val();
+    $(target).bind("input changes", function () {
+        const value = $(this).val();
 
-        if(value === data) {
+        if (value === data) {
             confirmBtn.removeAttr('disabled');
             return;
         }
@@ -70,12 +70,13 @@ function secureConfirmation(target, confirmBtn, data) {
     });
 }
 
-function clearInput(btn, target, toDisable ) {
+function clearInput(btn, target, toDisable) {
+
     btn.on('click', function (e) {
-        target.val("");
-        setTimeout(function() {
+        setTimeout(function () {
+            target.val("");
             toDisable.attr('disabled', true);
-        }, 3000);
+        }, 1);
     });
 }
 
