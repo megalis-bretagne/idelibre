a835fe1fa4e17f5ae4391f3764eb78d46c1b8fea
pmonin <pmonin@libriciel.coop>
2024-03-11 15:20:18 +0100

    rebase

--- src/Service/Csv/ExportUsersCsv.php
+++ src/Service/Csv/ExportUsersCsv.php
@@ -4,21 +4,24 @@ namespace App\Service\Csv;
 
 use App\Entity\Group;
 use App\Entity\Structure;
+use App\Repository\GroupRepository;
+use App\Repository\StructureRepository;
 use App\Repository\UserRepository;
-use App\Service\Export\ExportToZip;
 use App\Service\Util\Sanitizer;
 use League\Csv\CannotInsertRecord;
+use League\Csv\CharsetConverter;
 use League\Csv\Exception;
 use League\Csv\UnavailableStream;
 use League\Csv\Writer;
 use Symfony\Component\Filesystem\Filesystem;
+use ZipArchive;
 
 class ExportUsersCsv
 {
     public function __construct(
         private readonly UserRepository $userRepository,
-        private readonly Sanitizer $sanitizer,
         private readonly FileSystem $fileSystem,
+        private readonly Sanitizer $sanitizer,
     ) {
     }
 
@@ -34,8 +37,14 @@ class ExportUsersCsv
         $users = $this->userRepository->findByStructure($structure)->getQuery()->getResult();
 
         $pathDir = $this->csvPath();
+        $encoder = (new CharsetConverter())
+            ->inputEncoding('utf-8')
+            ->outputEncoding('utf-8')
+        ;
 
-        $csvWriter = Writer::createFromPath( $pathDir . '/' . $this->sanitizer->fileNameSanitizer($structure->getName(), 255) . '.csv', 'w+');
+
+        $csvWriter = Writer::createFromPath( $pathDir . '/' . $structure->getName() . '.csv', 'w+');
+        $csvWriter->addFormatter($encoder);
 
         foreach ($users as $user) {
             $csvWriter->insertOne(
@@ -74,12 +83,53 @@ class ExportUsersCsv
         };
     }
 
+
+    /**
+     * @throws UnavailableStream
+     * @throws CannotInsertRecord
+     * @throws Exception
+     */
+    public function exportGroupUsers(Group $group): string
+    {
+        $structuresPath = [];
+        foreach ($group->getStructures() as $structure) {
+            $structuresPath[] = $this->exportStructureUsers($structure);
+        }
+
+        return $this->genZipAndGetPath($structuresPath);
+    }
+
+    private function genZipAndGetPath(array $structuresPath): string
+    {
+        $zip = new ZipArchive();
+        $zipPath = '/tmp/' . uniqid('zip_report') . '.zip';
+        $zip->open($zipPath, ZipArchive::CREATE);
+        $zip->setCompressionName('deflate',ZipArchive::CM_DEFLATE);
+        $zip->setArchiveComment(ZipArchive::FL_ENC_UTF_8);
+
+        foreach ($structuresPath as $structurePath) {
+            $zip->addFile($structurePath);
+        }
+
+        $zip->close();
+
+        foreach ($structuresPath as $structurePath) {
+            $this->fileSystem->remove($structurePath);
+        }
+
+        return $zipPath;
+    }
+
+
+
     public function csvPath(): string
     {
         $pathFile = '/tmp';
 
         if (!$this->fileSystem->exists($pathFile)) {
-            $this->fileSystem->mkdir($pathFile, 0755);
+            $this->fileSystem->mkdir($pathFile, 0755, true);
+//            $this->fileSystem->chownS($pathFile, 'www-data');
+            $this->fileSystem->rename($pathFile, '/export');
         }
         return $pathFile;
     }
