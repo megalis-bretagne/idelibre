742534d06fafef0f0a446915dd1ed8b22ba981d0
pmonin <pmonin@libriciel.coop>
2024-03-26 15:43:34 +0100

    typo and details actor part 1

--- src/Form/AttendanceType.php
+++ src/Form/AttendanceType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Convocation;
 use App\Entity\User;
 use App\Repository\UserRepository;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
